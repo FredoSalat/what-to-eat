b567c66fadfb5db7825dde893011c3a12be9721f
FredoSalat <fredrik.marthinsens@gmail.com>
2023-12-09 15:58:13 +0100

    pentry page setup with components, small fix to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="text-center mb-4 text-gray-500 bottom-0 w-full mt-6">
+    <footer className="text-center mb-4 text-gray-500 fixed bottom-0 w-full mt-6">
       <small>
         &copy; {new Date().getFullYear()} What to eat!?. All rights reserved
       </small>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,15 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <div className="flex justify-between">
-      <h1 className="p-4 font-bold text-2xl">What to eat!?</h1>
+      <h1 className="p-4 font-bold text-2xl">
+        <Link href="/">What to eat!?</Link>
+      </h1>
       <ul className="p-4">
-        <li className="font-semibold">My pantry</li>
+        <li className="font-semibold">
+          <Link href="pantry">My pantry</Link>
+        </li>
       </ul>
     </div>
   );
